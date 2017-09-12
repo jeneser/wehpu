ed17eb4641b9656d6cfe663d1f2777d76ea17b23
jeneser <jeneserwang@gmail.com>
2017-09-12 17:31:12 +0800

    Coding for index, make some icons.

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,17 @@
 const app = getApp()
 
 Page({
-  data: {},
+  data: {
+    tools: [
+      { id: 'kbcx', name: '课表查询', disabled: false},
+      { id: 'kjs', name: '空教室', disabled: false},
+      { id: 'cjcx', name: '成绩查询', disabled: false},
+      { id: 'jyxx', name: '图书馆', disabled: false},
+      { id: 'ykt', name: '一卡通', disabled: false},
+      { id: 'bx', name: '报修', disabled: false},
+      { id: 'slj', name: '四六级', disabled: false},
+      { id: 'other', name: '更多', disabled: false}
+    ],
+  },
   onLoad: function () {}
 })
