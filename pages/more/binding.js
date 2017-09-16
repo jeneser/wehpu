5bc96546404b2a1c8b94af18046ce5560949db79
jeneser <jeneserwang@gmail.com>
2017-09-16 23:48:22 +0800

    Add binding step one two, need a succeed case.

--- pages/more/binding.js
+++ pages/more/binding.js
@@ -1,20 +1,39 @@
 Page({
   data: {
-    useridFocus: false
+    useridFocus: false,
+    vpnPassWordFocus: false,
+    jwcPassWordFocus: false,
+    helpStatus: false
   },
   // 获取焦点
   inputFocus: function (e) {
-    if (e.target.id == 'userid') {
+    if (e.target.id === 'userId') {
       this.setData({
-        'useridFocus': true
+        'userIdFocus': true
+      });
+    } else if (e.target.id === 'vpnPassWord') {
+      this.setData({
+        'vpnPassWordFocus': true
+      });
+    } else if (e.target.id === 'jwcPassWord') {
+      this.setData({
+        'jwcPassWordFocus': true
       });
     }
   },
   // 失去焦点
   inputBlur: function (e) {
-    if (e.target.id == 'userid') {
+    if (e.target.id === 'userId') {
       this.setData({
-        'useridFocus': false
+        'userIdFocus': false
+      });
+    } else if (e.target.id === 'vpnPassWord') {
+      this.setData({
+        'vpnPassWordFocus': false
+      });
+    } else if (e.target.id === 'jwcPassWord') {
+      this.setData({
+        'jwcPassWordFocus': false
       });
     }
   },
@@ -27,5 +46,18 @@ Page({
   // 取消认证
   navigateCancel: function() {
     wx.navigateBack();
+  },
+  // 帮助
+  showHelp: function() {
+    this.setData({
+      'helpStatus': true
+    });
+  },
+  hideHelp: function(e) {
+    if (e.target.id === "help" || e.target.id === "close-help") {
+      this.setData({
+        'helpStatus': false
+      });
+    }
   }
 })
