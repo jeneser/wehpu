c26627aa235a80ff162e8cbe0105798ad6c4df98
jeneser <jeneserwang@gmail.com>
2017-09-17 22:30:20 +0800

    Add course_tody page

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,19 +5,19 @@ const app = getApp()
 Page({
   data: {
     tools: [
-      { id: 'kbcx', name: '课表查询', disabled: false},
-      { id: 'kjs', name: '空教室', disabled: false},
-      { id: 'cjcx', name: '成绩查询', disabled: false},
-      { id: 'jyxx', name: '图书馆', disabled: false},
-      { id: 'ykt', name: '一卡通', disabled: false},
-      { id: 'bx', name: '报修', disabled: false},
-      { id: 'slj', name: '四六级', disabled: false},
-      { id: 'other', name: '更多', disabled: false}
+      { id: 'kbcx', name: '课表查询', url: '/pages/tools/course/course_tody', disabled: false},
+      { id: 'kjs', name: '空教室', url: '', disabled: false},
+      { id: 'cjcx', name: '成绩查询', url: '', disabled: false},
+      { id: 'jyxx', name: '图书馆', url: '', disabled: false},
+      { id: 'ykt', name: '一卡通', url: '', disabled: false},
+      { id: 'bx', name: '报修', url: '', disabled: false},
+      { id: 'slj', name: '四六级', url: '', disabled: false},
+      { id: 'other', name: '更多', url: '', disabled: false}
     ],
   },
-  onLoad: function() {
-    wx.navigateTo({
-      url: '/pages/more/binding'
-    });
-  }
+  // onLoad: function() {
+  //   wx.navigateTo({
+  //     url: '/pages/more/binding'
+  //   });
+  // }
 })
