d8c4e259b18b56e6036f030e8822594041354124
jeneser <jeneserwang@gmail.com>
2017-09-18 22:03:51 +0800

    Rename tools images, fix index

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,14 @@ const app = getApp()
 Page({
   data: {
     tools: [
-      { id: 'kbcx', name: '课表查询', url: '/pages/tools/course/course_tody', disabled: false},
-      { id: 'kjs', name: '空教室', url: '', disabled: false},
-      { id: 'cjcx', name: '成绩查询', url: '', disabled: false},
-      { id: 'jyxx', name: '图书馆', url: '', disabled: false},
-      { id: 'ykt', name: '一卡通', url: '', disabled: false},
-      { id: 'bx', name: '报修', url: '', disabled: false},
-      { id: 'slj', name: '四六级', url: '', disabled: false},
-      { id: 'other', name: '更多', url: '', disabled: false}
+      { id: 'course', name: '课表查询', url: '/pages/tools/course/course_tody', disabled: false},
+      { id: 'classroom', name: '空教室', url: '/pages/tools/classroom/classroom', disabled: false},
+      { id: 'score', name: '成绩查询', url: '/pages/tools/score/score', disabled: false},
+      { id: 'library', name: '图书馆', url: '/pages/tools/library/library', disabled: false},
+      { id: 'card', name: '一卡通', url: '/pages/tools/card/card', disabled: false},
+      { id: 'repair', name: '报修', url: '/pages/tools/repair/repair', disabled: false},
+      { id: 'cet', name: '四六级', url: '/pages/tools/cet/cet', disabled: false},
+      { id: 'more', name: '更多', url: '/pages/tools/more/more', disabled: false}
     ],
   },
   // onLoad: function() {
