0acc32c936639f7467df45a0a5963d9b6901ff10
jeneser <jeneserwang@gmail.com>
2017-09-19 18:47:49 +0800

    Add card page, e-card has some color issue

--- pages/tools/card/card.js
+++ pages/tools/card/card.js
@@ -1,4 +1,18 @@
 Page({
   data: {
-  }
+    helpStatus: false
+  },
+  // 帮助
+  showHelp: function () {
+    this.setData({
+      'helpStatus': true
+    });
+  },
+  hideHelp: function (e) {
+    if (e.target.id === "help" || e.target.id === "close-help") {
+      this.setData({
+        'helpStatus': false
+      });
+    }
+  },
 })
