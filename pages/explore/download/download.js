1b6b273a219604d6fa762079eafd2fcbfbacb171
jeneser <jeneserwang@gmail.com>
2017-09-22 23:06:39 +0800

    Add List template for download, more, user

--- pages/explore/download/download.js
+++ pages/explore/download/download.js
@@ -1,3 +1,30 @@
 Page({
-  data: {}
+  data: {
+    downloadList: [
+      {
+        image: '/images/file_icon/excel.png',
+        title: '2017年下半年全国大学英语四六级考试报名通知',
+        time: '2017-09-04',
+        rIcon: '/images/ic_download.png',
+        tapHandle: 'download'
+      },
+      {
+        image: '/images/file_icon/doc.png',
+        title: '2017年下半年全国大学英语四六级考试报名通知',
+        time: '2017-09-04',
+        rIcon: '/images/ic_download.png',
+        tapHandle: 'download'
+      },
+      {
+        image: '/images/file_icon/zip.png',
+        title: '2017年下半年全国大学英语四六级考试报名通知',
+        time: '2017-09-04',
+        rIcon: '/images/ic_download.png',
+        tapHandle: 'download'
+      }
+    ]
+  },
+  download: function() {
+    console.log(1);
+  }
 });
--- pages/more/more.js
+++ pages/more/more.js
@@ -1,4 +1,10 @@
 Page({
-  data: {},
-  onLoad: function() {}
+  data: {
+    userInfo: {
+      href: '/pages/more/user/user',
+      avatar: '/images/more/face.png',
+      nickname: 'jeneser',
+      bind: 0
+    }
+  }
 });
