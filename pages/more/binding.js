a1fdfba8b8ee804efbb7121080587752a7e1d76e
jeneser <jeneserwang@gmail.com>
2017-09-23 12:39:33 +0800

    Add tip, upload, help template

--- pages/more/binding.js
+++ pages/more/binding.js
@@ -1,15 +1,33 @@
 Page({
   data: {
-    useridFocus: false,
+    studentId: '',
+    studentIdFocus: false,
     vpnPassWordFocus: false,
     jwcPassWordFocus: false,
-    helpStatus: false
+    loading: false,
+    help: {
+      helpStatus: false,
+      faqList: [
+        {
+          question: '1.校外访问VPN密码是什么?',
+          answer: '初始密码或者已更改的密码。初始密码一般为身份证号后6位。'
+        },
+        {
+          question: '2.教务处密码是什么?',
+          answer: '初始密码或者已更改的密码。初始密码为学号/一卡通号。'
+        },
+        {
+          question: '3.忘记密码?',
+          answer: '请联系学校教务处管理员。'
+        }
+      ]
+    }
   },
   // 获取焦点
   inputFocus: function(e) {
-    if (e.target.id === 'userId') {
+    if (e.target.id === 'studentId') {
       this.setData({
-        userIdFocus: true
+        studentIdFocus: true
       });
     } else if (e.target.id === 'vpnPassWord') {
       this.setData({
@@ -23,9 +41,9 @@ Page({
   },
   // 失去焦点
   inputBlur: function(e) {
-    if (e.target.id === 'userId') {
+    if (e.target.id === 'studentId') {
       this.setData({
-        userIdFocus: false
+        studentIdFocus: false
       });
     } else if (e.target.id === 'vpnPassWord') {
       this.setData({
@@ -47,16 +65,42 @@ Page({
   navigateCancel: function() {
     wx.navigateBack();
   },
+  // 扫码
+  scanStudentId: function() {
+    wx.scanCode({
+      onlyFromCamera: true,
+      success: res => {
+        console.log(res);
+        this.setData({
+          studentId: res.result
+        });
+      }
+    });
+  },
+  showTip: function() {
+    wx.showModal({
+      title: '提示',
+      content: '请将学生卡/一卡通背面的条形码放入方框内。',
+      success: res => {
+        if (res.confirm) {
+          console.log('用户点击确定');
+          this.scanStudentId();
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
+  },
   // 帮助
   showHelp: function() {
     this.setData({
-      helpStatus: true
+      'help.helpStatus': true
     });
   },
   hideHelp: function(e) {
     if (e.target.id === 'help' || e.target.id === 'close-help') {
       this.setData({
-        helpStatus: false
+        'help.helpStatus': false
       });
     }
   },
