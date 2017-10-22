da2ea86ef6230aaf93e187e307dd12f8f269dea3
jeneser <jeneserwang@gmail.com>
2017-10-23 06:20:03 +0800

    Fix index course view

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,63 +5,81 @@ Page({
     bind: false,
     // 工具
     tools: [
-      {
-        id: 'course',
-        name: '课表查询',
-        url: '/pages/tools/course/course'
-      },
-      {
-        id: 'classroom',
-        name: '空教室',
-        url: '/pages/tools/classroom/classroom'
-      },
-      {
-        id: 'score',
-        name: '成绩查询',
-        url: '/pages/tools/score/score'
-      },
-      {
-        id: 'library',
-        name: '图书馆',
-        url: '/pages/tools/library/library'
-      },
-      {
-        id: 'card',
-        name: '一卡通',
-        url: '/pages/tools/card/card'
-      },
-      {
-        id: 'repair',
-        name: '报修',
-        url: '/pages/tools/repair/repair'
-      },
-      {
-        id: 'cet',
-        name: '座位预约',
-        url: '/pages/tools/seat/seat'
-      },
-      { id: 'more', name: '更多', url: '/pages/tools/more/more' }
+      [
+        {
+          id: 'course',
+          name: '课表查询',
+          url: '/pages/tools/course/course'
+        },
+        {
+          id: 'classroom',
+          name: '空教室',
+          url: '/pages/tools/classroom/classroom'
+        },
+        {
+          id: 'score',
+          name: '成绩查询',
+          url: '/pages/tools/score/score'
+        },
+        {
+          id: 'library',
+          name: '图书馆',
+          url: '/pages/tools/library/library'
+        },
+        {
+          id: 'card',
+          name: '一卡通',
+          url: '/pages/tools/card/card'
+        },
+        {
+          id: 'repair',
+          name: '报修',
+          url: '/pages/tools/repair/repair'
+        },
+        {
+          id: 'cet',
+          name: '座位预约',
+          url: '/pages/tools/seat/seat'
+        },
+        {
+          id: 'cet',
+          name: '体测查询',
+          url: '/pages/tools/seat/seat'
+        }
+      ],
+      [
+        {
+          id: 'card',
+          name: '一卡通',
+          url: '/pages/tools/card/card'
+        },
+        {
+          id: 'repair',
+          name: '报修',
+          url: '/pages/tools/repair/repair'
+        }
+      ]
     ],
     // 当前课程
     currentCourse: [
-      // {
-      //   name: '数据库系统原理',
-      //   room: '202',
-      //   place: '计算机综合楼',
-      //   count: '2',
-      //   teacher: '刘老师',
-      //   time: '8:00-10:00',
-      //   state: 1
-      // },
-      // {
-      //   name: '数据库系统原理',
-      //   room: '202',
-      //   place: '计算机综合楼',
-      //   count: '2',
-      //   teacher: '刘老师',
-      //   time: '8:00-10:00',
-      //   state: 0
-      // }
+      {
+        name: '数据库系统原理',
+        room: '202',
+        place: '计算机综合楼',
+        count: '2',
+        teacher: '刘老师',
+        time: '8:00-10:00',
+        state: 1
+      },
+      {
+        name: '数据库系统原理',
+        room: '202',
+        place: '计算机综合楼',
+        count: '2',
+        teacher: '刘老师',
+        time: '8:00-10:00',
+        state: 0
+      }
     ]
   },
   onLoad: function() {
