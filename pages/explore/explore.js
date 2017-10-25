e377e4f879670c346100090e3a16920078d033d6
jeneser <jeneserwang@gmail.com>
2017-10-25 11:46:50 +0800

    重构发现活动页面

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -2,19 +2,25 @@ Page({
   data: {
     topicList: [
       {
-        href: '',
-        cover: '/images/cover/cover_1.jpg',
         title: '世界这么大,我们出去走走吧!',
-        author: '创意俱乐部',
-        time: '9月17日'
+        tag: '创意俱乐部',
+        cover: '/images/cover/cover_1.jpg'
       },
       {
-        href: '',
-        cover: '/images/cover/cover_1.jpg',
         title: '世界这么大,我们出去走走吧!',
-        author: '创意俱乐部',
-        time: '9月17日'
-      }
+        tag: '创意俱乐部',
+        cover: '/images/cover/cover_1.jpg',
+      },
+      {
+        title: '世界这么大,我们出去走走吧!',
+        tag: '创意俱乐部',
+        cover: '/images/cover/cover_1.jpg',
+      },
+      {
+        title: '世界这么大,我们出去走走吧!',
+        tag: '创意俱乐部',
+        cover: '/images/cover/cover_1.jpg',
+      },
     ]
   }
 });
