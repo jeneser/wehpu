4b6a2602d2740da3b5b0f334912098bc43a1b01e
jeneser <jeneserwang@gmail.com>
2017-10-28 14:19:41 +0800

    添加捐赠致谢页面

--- pages/more/about/about.js
+++ pages/more/about/about.js
@@ -1,3 +1,31 @@
+var app = getApp();
+
 Page({
-  data: {}
+  data: {
+    version: ''
+  },
+  onLoad: function() {
+    this.getVersion();
+  },
+
+  // 获取版本
+  getVersion: function() {
+    this.setData({
+      version: app.version
+    });
+  },
+
+  // 设置剪切板
+  setClipboardData: function(e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.content,
+      success: () => {
+        wx.showToast({
+          title: '内容已复制',
+          icon: 'success',
+          duration: 2000
+        });
+      }
+    });
+  }
 });
---
+++ pages/more/about/donate.js
@@ -0,0 +1,51 @@
+var app = getApp();
+
+Page({
+  data: {
+    mode: 'wechatpay',
+    donorList: [
+      {
+        donor: '可爱的二哈',
+        money: '20'
+      },
+      {
+        donor: '可爱的二哈',
+        money: '20'
+      },
+      {
+        donor: '可爱的二哈',
+        money: '20'
+      }
+    ]
+  },
+  // 切换方式
+  switchMode: function(e) {
+    if (e.target.id) {
+      this.setData({
+        mode: e.target.id
+      });
+    }
+  },
+  // 保存二维码
+  saveQr: function(e) {
+    var path = '/images/more/' + this.data.mode + '_qrcode.png';
+
+    wx.saveImageToPhotosAlbum({
+      filePath: path,
+      success: () => {
+        wx.showToast({
+          title: '已保存到本地',
+          icon: 'success',
+          duration: 2000
+        });
+      },
+      fail: () => {
+        wx.showToast({
+          title: '自动保存失败',
+          image: '/images/common/fail.png',
+          duration: 2000
+        });
+      }
+    });
+  }
+});
